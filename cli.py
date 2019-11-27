73647f3a57241b9f27c1bd6ec1eb5f4aa9f4eefa
Daniel Stotts <dstodev@gmail.com>
2019-11-27 00:00:13 -0800

    Sorted CLI output by highest multiplier first

--- cli.py
+++ cli.py
@@ -5,10 +5,10 @@ from src.typearray import TypeList
 
 
 def get_parameters():
-    parser = ap.ArgumentParser(description="Enter types and this program will output recommended type matchups.")
+    parser = ap.ArgumentParser(description="Enter types and this program will output type matchups")
 
-    parser.add_argument("types", metavar="TYPE", type=str.capitalize, nargs="+", help="Types to calculate matchups against.",
-                        choices=[type_.name for type_ in PokeType])
+    parser.add_argument("types", metavar="TYPE", type=str.capitalize, nargs="+",
+                        help="Types to calculate matchups against", choices=[type_.name for type_ in PokeType])
 
     return parser.parse_args()
 
@@ -18,12 +18,13 @@ if __name__ == "__main__":
 
     types = [PokeType[type_name] for type_name in args.types]
     types = TypeList(types)
-    weaknesses = types.get_all_interactions(False)
+    interactions = types.get_all_interactions(False)
+    interactions = {k: interactions[k] for k in sorted(interactions, key=interactions.get, reverse=True)}
 
     all_types = [type_.name for type_ in PokeType]
     type_name_width = len(max(all_types, key=len))
 
     print(f"\nSelected types are: {', '.join(args.types)}")
     print(f"Multipliers against these types:")
-    for weakness, multiplier in weaknesses.items():
+    for weakness, multiplier in interactions.items():
         print(f"    {weakness.name:<{type_name_width}} {multiplier}")
